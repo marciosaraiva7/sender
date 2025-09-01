cad1837b0fc99ba8c1de858af53ae5659cc08376
marciosaraiva7 <marciohm7@gmail.com>
2025-09-01 14:12:02 -0300

    Atualiza layout da aplicação para incluir configuração de PWA, definindo URL base e adicionando suporte a web app da Apple com imagem de inicialização. Melhora a estrutura de metadata para melhor compatibilidade com dispositivos móveis.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
+import PwaRegister from "@/components/pwa-register";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
-import PwaRegister from "@/components/pwa-register";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,13 +14,27 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const defaultUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
 export const metadata: Metadata = {
+  metadataBase: new URL(defaultUrl),
   title: "Sender IA",
   description: "Aplicação para enviar arquivos CSV via webhook",
   manifest: "/manifest.webmanifest",
   icons: {
     icon: "/logo-sender.png",
     apple: "/logo-sender.png",
+  },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    startupImage: [
+      {
+        url: "/netune.png",
+        media:
+          "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)",
+      },
+    ],
   },
 };
 
